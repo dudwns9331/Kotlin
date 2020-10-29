8bc0fa94954b8ce39ae80ef7e0d6282fc7b1221d
dudwns9331 <dudwns1045@naver.com>
2020-10-29 13:51:30 +0900

    2019/07/28

---
+++ chapter02/section1/defaultPackage.kt
@@ -0,0 +1,19 @@
+package chap02.section1
+
+import com.example.edu.Person
+import kotlin.math.PI
+import kotlin.math.abs
+
+fun main() {
+    val intro: String = "안녕하세요"
+    val num : Int = 20
+    val user1 = Person("Kildong", 30)
+
+    println(user1.name)
+    println(user1.age)
+
+//    println(PI)
+//    println(abs(-12.6)) //절대값을 출력하는 abs() 함수
+//
+//    println("intro : $intro, num: $num")
+}
---
+++ chapter02/자료형.kt
@@ -0,0 +1,9 @@
+package chap02
+
+val num1: Byte = 127
+val num2 = -123123127
+val num3 = 123123123127
+val num4 = 1231231312313121127
+
+
+
