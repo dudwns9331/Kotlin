f5658404afff0b1a2d433daa5d3b794af7637bdf
dudwns9331 <dudwns1045@naver.com>
2020-11-05 15:36:52 +0900

    2020/08/12

---
+++ chapter04/section1/else if문을 사용한 등급 판별하기.kt
@@ -0,0 +1,25 @@
+package chap04.section1
+
+import java.util.*
+
+fun main() {
+    print("Enter the score: ")
+
+    val score = readLine()!!.toDouble()
+    var grade : Char = 'F'
+
+    if (score >= 90.0)
+        grade = 'A'
+    else if (score >= 80.0 && score <= 89.9)
+        grade = 'B'
+    else if (score >= 70.0 && score <= 79.9)
+        grade = 'C'
+
+    println("Score : $score, Grade : $grade")
+
+    var a = Scanner(System.`in`)
+
+    var b : Int = a.nextInt()
+
+    println(b)
+}
