e6914e31d52b0e75737022b6f90ef6dba244e314
dudwns9331 <dudwns1045@naver.com>
2020-11-18 15:38:10 +0900

    2019/08/21

---
+++ DMRL_Kotlin/week1/p2.kt
@@ -0,0 +1,15 @@
+package DMRL.week1
+
+import java.util.*
+
+fun main() {
+    print("단어를 두 개 입력하시오 : ")
+
+    var input = Scanner(System.`in`)
+
+    var a : String = input.next()
+    var b : String = input.next()
+
+    println(a+b)
+
+}
