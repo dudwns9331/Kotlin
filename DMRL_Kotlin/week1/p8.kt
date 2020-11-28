cab19144991307b0e891ebd96fa2413c358af20c
dudwns9331 <dudwns1045@naver.com>
2020-11-28 11:19:00 +0900

    2019/08/27

---
+++ DMRL_Kotlin/week1/p8.kt
@@ -0,0 +1,16 @@
+package DMRL.week1
+
+import java.util.*
+
+fun main() {
+    var input = Scanner(System.`in`)
+
+    println("1부터 얼마까지의 짝수를 더하시겠습니까? ")
+
+    var n = input.nextInt()
+    var sum : Int = 0
+
+
+
+    println("첫 방법 : 1부터 $n 까지의 짝수의 합 = $sum")
+}
