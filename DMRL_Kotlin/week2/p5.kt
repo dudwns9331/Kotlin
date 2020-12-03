42e7f0f0b83c5a72c9488739d6e1746af59c967f
dudwns9331 <dudwns1045@naver.com>
2020-12-03 19:37:49 +0900

    2019/09/01

---
+++ DMRL_Kotlin/week2/p5.kt
@@ -0,0 +1,12 @@
+package DMRL.week2
+
+import java.util.*
+
+fun main() {
+    var input = Scanner(System.`in`)
+    print("양의 정수를 하나 입력하시오 : ")
+    var n = input.nextInt()
+
+    for(i in n downTo 1)
+    print("$i ")
+}
